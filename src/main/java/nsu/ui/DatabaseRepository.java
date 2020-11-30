8fd26abd7173254f7d340055abd29c37397d8599
mashabat2 <mashabat2@mail.ru>
2020-11-30 22:05:39 +0700

    Didn't finish findAll()

--- src/main/java/nsu/ui/DatabaseRepository.java
+++ src/main/java/nsu/ui/DatabaseRepository.java
@@ -1,16 +1,57 @@
 package nsu.ui;
-
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ArrayList;
 
-public class DatabaseRepository implements TestRepository{
+public class DatabaseRepository implements TestRepository {
+    private static Connection connection;
+    private static Statement statement;
+    private static ResultSet result;
 
     @Override
     public ArrayList<Tests> findAll() {
-        ArrayList<Tests> tests = new ArrayList<>();
-        return tests;
+        ArrayList<Tests> test = new ArrayList<Tests>();
+        ArrayList<Tests> testNames = new ArrayList<>();
+        HashMap<Long, String> testTeacher = new HashMap<Long, String>();
+        String selectSQL = "SELECT id, second_name from teachers";
+        try {
+            result = statement.executeQuery(selectSQL);
+            while (result.next()) {
+                testTeacher.put(result.getLong(1), result.getString(2));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        String selectSql = "SELECT test_name from tests";
+
+        try {
+            result = statement.executeQuery(selectSql);
+        } catch (SQLException e) {
+            System.out.println("Ошибка Select");
+        }
+        try {
+            while (result.next()) {
+                Tests tests = new Tests();
+                tests.setId(result.getLong(1));
+                tests.setTeacher(testTeacher.);
+                tests.setQuestion(result.getString(3));
+                tests.setTestName(result.);
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return test;
     }
 
-    @Override
+
+        @Override
     public Tests save(Tests test) {
         return null;
     }
